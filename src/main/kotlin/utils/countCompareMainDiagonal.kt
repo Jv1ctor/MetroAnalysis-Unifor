435a6025839e6e21481df4e2e02f385249daf7ae
Jv1ctor <j.rodriguesbarretos@gmail.com>
2025-11-02 19:24:07 -0300

    fix: compare for value

--- src/main/kotlin/utils/countCompareMainDiagonal.kt
+++ src/main/kotlin/utils/countCompareMainDiagonal.kt
@@ -2,11 +2,11 @@ package org.example.utils
 
 import Linear.Matrices.Matrix
 
-fun countCompareMainDiagonal(project: Matrix, value: Float): Int{
+fun countCompareMainDiagonal(project: Matrix, compareValue: Float): Int{
     var countEquals = 0
     for(i in 0 until project.rows){
         val value = project.get(i, i)
-        if(value == value){
+        if(value == compareValue){
             countEquals++;
         }
     }
