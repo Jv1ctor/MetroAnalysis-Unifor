10f830f5a2396850fefece1d6517138ece7a5134
Jv1ctor <j.rodriguesbarretos@gmail.com>
2025-11-02 20:41:23 -0300

    feat: add typed of relations by Vitor Dantas

--- src/main/kotlin/typedRelation/isRelationEquivalence.kt
+++ src/main/kotlin/typedRelation/isRelationEquivalence.kt
@@ -1,4 +1,18 @@
 package org.example.typedRelation
 
-class isRelationEquivalence {
+import Linear.Matrices.Matrix
+import org.example.relationFunctions.isReflective
+import org.example.relationFunctions.isSymmetrical
+import org.example.relationFunctions.isTransitive
+
+fun isRelationEquivalence(project: Matrix): Boolean {
+
+    val t = isReflective(project)
+    val a = isSymmetrical(project)
+    val d = isTransitive(project)
+    if (isReflective(project) && isSymmetrical(project) && isTransitive(project)) {
+        return true
+    }
+
+    return false
 }
--- src/main/kotlin/typedRelation/isRelationOrder.kt
+++ src/main/kotlin/typedRelation/isRelationOrder.kt
@@ -1,4 +1,14 @@
 package org.example.typedRelation
 
-class isRelationOrder {
+import Linear.Matrices.Matrix
+import org.example.relationFunctions.isAsymmetric
+import org.example.relationFunctions.isReflective
+import org.example.relationFunctions.isTransitive
+
+fun isRelationOrder(project: Matrix): Boolean {
+    if (isReflective(project) && isAsymmetric(project) && isTransitive(project)) {
+        return true
+    }
+
+    return false
 }
