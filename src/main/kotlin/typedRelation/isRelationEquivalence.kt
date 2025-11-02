894b536f03bbd153a367f6fc229fb8c8116f15a8
Jv1ctor <j.rodriguesbarretos@gmail.com>
2025-11-02 20:52:59 -0300

    fix: remove debug

--- src/main/kotlin/typedRelation/isRelationEquivalence.kt
+++ src/main/kotlin/typedRelation/isRelationEquivalence.kt
@@ -6,10 +6,6 @@ import org.example.relationFunctions.isSymmetrical
 import org.example.relationFunctions.isTransitive
 
 fun isRelationEquivalence(project: Matrix): Boolean {
-
-    val t = isReflective(project)
-    val a = isSymmetrical(project)
-    val d = isTransitive(project)
     if (isReflective(project) && isSymmetrical(project) && isTransitive(project)) {
         return true
     }
